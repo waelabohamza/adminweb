1d9d3b2d367274695bc9e3452d1d0a333842a90b
waelabohamza <waeleagle1243@gmail.com>
2021-03-03 22:27:00 +0200

    refresh

--- api/services/services.php
+++ api/services/services.php
@@ -1,8 +1,10 @@
 <?php
 include "../../connect.php";
+
 $table = "servciesview";
 
 $limit = paginationLimit($_GET['page'] ?? null, 10);
 
 $data = getAllData($table, "1 = 1  $limit ");
+
 createJson($data['count'], $data['values']);
