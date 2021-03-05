fad034e9723dff428164b4e64ad8ba03bd158edd
waelabohamza <waeleagle1243@gmail.com>
2021-03-05 12:12:51 +0200

    refresh

--- api/home.php
+++ api/home.php
@@ -1,65 +1,58 @@
-<?php 
-  
-  include "../connect.php" ; 
+<?php
+
+include "../connect.php";
 
 
-  $arraydatahome = array(); 
+$arraydatahome = array();
 
-  $data = getAllData("servciesview" , "services_favorite = 1 LIMIT 6 "); 
+$data = getAllData("servciesview", "services_favorite = 1 LIMIT 6 ");
 
- if ( $data['count'] > 0 ) {
-   
-    $arraydatahome['servicesfavorite'] = $data['values'] ; 
+if ($data['count'] > 0) {
 
- }
+   $arraydatahome['servicesfavorite'] = $data['values'];
+}
 
- $data = getAllData("servciesview" , "services_common = 1 LIMIT 5 "); 
-  
-
- if ( $data['count'] > 0 ) {
-   
-    $arraydatahome['servicescommon'] = $data['values'] ; 
-
- }
-
- $data = getAllData("expertsview" , "experts_common = 1 LIMIT 5"); 
-
- if ( $data['count'] > 0 ) {
-   
-    $arraydatahome['expertscommon'] = $data['values'] ; 
-
- }
-
- $data = getAllData("questions" , "questions_common = 1 LIMIT 5"); 
-
- if ( $data['count'] > 0 ) {
-   
-    $arraydatahome['questionscommon'] = $data['values'] ; 
-
- }
-
- $data = getAllData("coursesview" , "courses_common = 1 LIMIT 5"); 
-
- if ( $data['count'] > 0 ) {
-   
-    $arraydatahome['coursescommon'] = $data['values'] ; 
-
- }
+$data = getAllData("servciesview", "services_common = 1 LIMIT 5 ");
 
 
- $data = getAllData("categories" , "1 = 1 LIMIT 5 "); 
+if ($data['count'] > 0) {
 
- if ( $data['count'] > 0 ) {
-   
-    $arraydatahome['categories'] = $data['values'] ; 
+   $arraydatahome['servicescommon'] = $data['values'];
+}
 
- }
+$data = getAllData("expertsview", "experts_common = 1 LIMIT 5");
 
- 
+if ($data['count'] > 0) {
+
+   $arraydatahome['expertscommon'] = $data['values'];
+}
+
+$data = getAllData("questions", "questions_common = 1 LIMIT 5");
+
+if ($data['count'] > 0) {
+
+   $arraydatahome['questionscommon'] = $data['values'];
+}
+
+$data = getAllData("coursesview", "courses_common = 1 LIMIT 5");
+
+if ($data['count'] > 0) {
+
+   $arraydatahome['coursescommon'] = $data['values'];
+}
+
+
+$data = getAllData("categories", "1 = 1 LIMIT 5 ");
+
+if ($data['count'] > 0) {
+
+   $arraydatahome['categories'] = $data['values'];
+}
+
+
 
 // echo "<pre>" ; 
+echo json_encode($arraydatahome); 
+
 // print_r($arraydatahome) ; 
 // echo "</pre>" ; 
-
-
-echo json_encode($arraydatahome) ; 
