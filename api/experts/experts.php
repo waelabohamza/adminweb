0373b2cbc245f1fdd91f7458dd8d2b0ee1b17c50
waelabohamza <waeleagle1243@gmail.com>
2021-03-10 19:57:54 +0200

    refresh

--- api/experts/experts.php
+++ api/experts/experts.php
@@ -11,14 +11,14 @@ if (isset($_POST['id'])) {
 
     $id = $_POST['id'];
 
-    $and = "AND services_categories = '$id' ";
+    $and = "AND experts_cat = '$id' ";
 
-    $and .= "AND  services_name like '%$search%' ";
+    $and .= "AND ( experts_name like '%$search%' OR  experts_name_ar like '%$search%' ) ";
 }
 
 
 $limit = paginationLimit($_GET['page'] ?? null, 10);
 
-$data = getAllData($table, "1 = 1  $limit ");
+$data = getAllData($table, "1 = 1   $and  ");
 
 createJson($data['count'], $data['values']);
