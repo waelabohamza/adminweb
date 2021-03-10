2925270de92f4ad75087c3ae03ecf74956696477
waelabohamza <waeleagle1243@gmail.com>
2021-03-10 22:06:03 +0200

    refresh

--- api/questions/questions.php
+++ api/questions/questions.php
@@ -1,6 +1,19 @@
 <?php
 include "../../connect.php";
 $table = "questions";
+
+$and = null;
+
+if (isset($_POST['id'])) {
+
+    $search = $_POST['search'];
+
+    $id = $_POST['id'];
+
+    $and = "AND  ( questions_name like '%$search%' OR questions_name_ar  like '%$search%')  ";
+}
+
+
 $limit = paginationLimit($_GET['page'] ?? null, 10);
 $data = getAllData($table, "1 = 1  $limit ");
-createJson( $data['count'], $data['values']) ; 
+createJson($data['count'], $data['values']);
