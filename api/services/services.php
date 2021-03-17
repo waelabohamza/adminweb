e41a3ccd85016c3ac8dc6796fbc97ef62c0f2391
waelabohamza <waeleagle1243@gmail.com>
2021-03-17 19:14:09 +0200

    refresh

--- api/services/services.php
+++ api/services/services.php
@@ -4,24 +4,25 @@ include "../../connect.php";
 
 $table = "servciesview";
 
-$and = null ;
+$and = null;
+
 
 $limit = paginationLimit($_GET['page'] ?? null, 1000);
 
-
 if (isset($_POST['id'])) {  // For  Search Services in page ( services  ) in app  
 
-    $search = $_POST['search'] ; 
+    $search = $_POST['search'];
 
     $id = $_POST['id'];
 
     $and = "AND services_categories = '$id' ";
 
-    $and .= "AND  services_name like '%$search%' " ; 
-
+    $and .= "AND  services_name like '%$search%' ";
 }
- 
 
+if (isset($_POST['dropdownsearch'])) {
+    $limit = "LIMIT 10000";
+}
 
 
 $data = getAllData($table, "1 = 1  $and  $limit");
