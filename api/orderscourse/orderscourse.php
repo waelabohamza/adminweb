3de71fa63fabe8a38eed49bf858638afb83af766
waelabohamza <waeleagle1243@gmail.com>
2021-03-17 16:38:12 +0200

    refresh

--- api/orderscourse/orderscourse.php
+++ api/orderscourse/orderscourse.php
@@ -2,14 +2,20 @@
 
 include "../../connect.php";
 
+
 $table = "orderscourseview";
+
 
 $and = null;
 
+
 $userid = $_POST['userid'];
+
 
 $limit = paginationLimit($_GET['page'] ?? null, 10);
 
+
 $data = getAllData($table, "users_id = '$userid'  $and  ORDER BY orderscourse_id DESC  $limit");
+
 
 createJson($data['count'], $data['values']);
