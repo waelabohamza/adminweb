82a74ef0e364b6f1afdeb61bf339ab5072cf1a87
waelabohamza <waeleagle1243@gmail.com>
2021-03-17 10:55:37 +0200

    refresh

--- api/ordersservice/ordersservice.php
+++ api/ordersservice/ordersservice.php
@@ -9,9 +9,9 @@ $and = null;
 
 $userid = $_POST['userid'] ;  
  
-$limit = paginationLimit($_GET['page'] ?? null, 10);
+$limit = paginationLimit($_GET['page'] ?? null, 100);
 
-$data = getAllData($table, "users_id = '$userid'  $and  $limit");
+$data = getAllData($table, "users_id = '$userid'  $and  ORDER BY ordersservice_id DESC  $limit");
 
 createJson($data['count'], $data['values']);
 
