9aa7de1ecd14876167e374d93235d37a104c0d5a
waelabohamza <waeleagle1243@gmail.com>
2021-03-19 21:49:16 +0200

    refresh

--- api/services/services.php
+++ api/services/services.php
@@ -6,25 +6,28 @@ $table = "servciesview";
 
 $and = null;
 
-
 $limit = paginationLimit($_GET['page'] ?? null, 1000);
 
-if (isset($_POST['id'])) {  // For  Search Services in page ( services  ) in app  
+        if (isset($_POST['id'])) {  // For  Search Services in page ( services  ) in app  
+         
+            $search  = $_POST['search'];
 
-    $search = $_POST['search'];
+            $id      = $_POST['id'];
 
-    $id = $_POST['id'];
+            $and     = "AND services_categories = '$id' ";
 
-    $and = "AND services_categories = '$id' ";
+            $and     .= "AND  services_name like '%$search%' ";
+      
+        }
 
-    $and .= "AND  services_name like '%$search%' ";
-}
-
-if (isset($_POST['dropdownsearch'])) {
-    $limit = "LIMIT 10000";
-}
-
+        if (isset($_POST['dropdownsearch'])) {
+         
+            $limit = "LIMIT 10000";
+        
+        }
 
 $data = getAllData($table, "1 = 1  $and  $limit");
 
 createJson($data['count'], $data['values']);
+
+
