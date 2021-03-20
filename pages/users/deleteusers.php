d2ede429603e79bef0d822c37c1a41f2a31bb8e9
waelabohamza <waeleagle1243@gmail.com>
2021-03-21 00:28:26 +0200

    refresh

---
+++ pages/users/deleteusers.php
@@ -0,0 +1,52 @@
+<?php include "../../ini.php";  ?>
+<?php include "../../include/header.php"; ?>
+<?php include "../../include/navmobile.php";   ?>
+
+
+<!-- Start Body  -->
+
+<div class="container-fluid">
+
+    <div class="row">
+
+        <?php include "../../include/menutop.php";  ?>
+
+    </div>
+    <!-- Edn Menu-top -->
+
+
+    <!-- Start SideBar And Body Page -->
+
+    <div class="row">
+
+        <!-- Start SideBar  -->
+        <div class="col-md-4 col-lg-2 hidden-xs hidden-sm sidebar">
+
+            <?php include "../../include/sidebar.php";   ?>
+
+        </div>
+        <!-- End SideBar  -->
+
+        <!-- Start SideBar  -->
+        <div class="col-xs-12  col-md-8 col-lg-10">
+
+            <!-- start Body =================================== -->
+
+            <!-- End Body ======================================= -->
+
+        </div>
+
+
+
+    </div>
+    <!-- End SideBar And Body Page -->
+
+
+</div>
+
+
+<!-- End Body  -->
+
+
+
+<?php include "../../include/footer.php";  ?>
