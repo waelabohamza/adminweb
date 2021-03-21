0ce4dc82098a96c91c617aa3cfd546725fcbc21e
waelabohamza <waeleagle1243@gmail.com>
2021-03-21 11:25:36 +0200

    refresh

--- pages/categories/addcategories.php
+++ pages/categories/addcategories.php
@@ -1,97 +1,95 @@
-<?php include "../../ini.php";  ?>
+<?php
+ob_start();
+include "../../ini.php";  ?>
 <?php include "../../include/header.php"; ?>
 <?php include "../../include/navmobile.php";   ?>
-
-
 <!-- Start Body  -->
 
 <div class="container-fluid">
 
-    <div class="row">
+  <div class="row">
 
-        <?php include "../../include/menutop.php";  ?>
+    <?php include "../../include/menutop.php";  ?>
+
+  </div>
+  <!-- Edn Menu-top -->
+
+
+  <!-- Start SideBar And Body Page -->
+
+  <div class="row">
+
+    <!-- Start SideBar  -->
+    <div class="col-md-4 col-lg-2 hidden-xs hidden-sm sidebar">
+
+      <?php include "../../include/sidebar.php";   ?>
 
     </div>
-    <!-- Edn Menu-top -->
+    <!-- End SideBar  -->
+
+    <!-- Start SideBar  -->
+    <div class="col-xs-12  col-md-8 col-lg-10">
+
+      <!-- start Body =================================== -->
+      <?php
 
 
-    <!-- Start SideBar And Body Page -->
+      $do  = isset($_GET['do']) ?   $_GET['do'] : "add";
 
-    <div class="row">
-    
-        <!-- Start SideBar  -->
-        <div class="col-md-4 col-lg-2 hidden-xs hidden-sm sidebar">
+      if ($do  == "add") {
 
-            <?php include "../../include/sidebar.php";   ?>
+      ?>
 
+
+        <div class="panel panel-default panel-custom">
+          <div class="panel-heading">
+            <h3 class="panel-title">Add Users</h3>
+          </div>
+          <div class="panel-body">
+
+            <form enctype="multipart/form-data" autocomplete="off" class="form-horizontal addvalidate" action="<?php echo $_SERVER['PHP_SELF'] . '?do=insert'; ?>" method="post">
+              <div class="form-group">
+                <label for="name" class="col-sm-2 control-label">category name</label>
+                <div class="col-sm-10">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="name">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="namear" class="col-sm-2 control-label">category name arabic</label>
+                <div class="col-sm-10">
+                  <input type="text" name="namear" class="form-control" id="namear" placeholder="namear">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="desc" class="col-sm-2 control-label">description</label>
+                <div class="col-sm-10">
+                  <input type="text" name="desc" class="form-control" id="desc" placeholder="desc">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="descar" class="col-sm-2 control-label">description arabic</label>
+                <div class="col-sm-10">
+                  <input type="text" name="descar" class="form-control" id="descar" placeholder="descar">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-success btn-sm"> <i class="fa fa-plus"></i> Add Category </button>
+                </div>
+              </div>
+            </form>
+
+          </div>
         </div>
-        <!-- End SideBar  -->
-
-        <!-- Start SideBar  -->
-        <div class="col-xs-12  col-md-8 col-lg-10">
-
-            <!-- start Body =================================== -->
-
-<div class="panel panel-default panel-custom">
-        <div class="panel-heading">
-            <h3 class="panel-title">Add Categories</h3>
-        </div>
-           <div class="panel-body">
-
-              <form  enctype="multipart/form-data" autocomplete="off" 
-              class="form-horizontal addvalidate" action="<?php echo $_SERVER['PHP_SELF'] . '?do=insert';?>" method="post">
-                <div class="form-group">
-                  <label for="name" class="col-sm-2 control-label">name</label>
-                  <div class="col-sm-10">
-                    <input type="text" name="stdname" class="form-control" id="name" 
-                    placeholder="name">
-                  </div>
-                </div>
-               
-                 <div class="form-group">
-                  <label for="religion" class="col-sm-2 control-label">religion</label>
-                  <div class="col-sm-10">
-                    <select class="niceselect wide" name="stdreligion">
-                       <option value="0">...</option>
-                       <option value="1">Muslim</option>
-                       <option value="2">Christian</option>
-                       <option value="2">Jewish</option>
-                       <option value="4">Atheist</option>
-                       <option value="5">other</option>
-                    </select>
-                  </div>
-                </div>
-               
-                
-                 <div class="form-group">
-                  <label for="image" class="col-sm-2 control-label">image</label>
-                  <div class=" file-upload col-sm-10">
-                    <div class="file-select">
-                      <div class="file-select-button" id="fileName">Choose image</div>
-                      <div class="file-select-name" id="noFile">No image chosen...</div> 
-                      <input type="file" name="stdimage" id="chooseFile" placeholder="image">
-                    </div>
-                  </div>
-                </div>
-        
-                <div class="form-group">
-                  <div class="col-sm-offset-2 col-sm-10">
-                    <button type="submit" class="btn btn-success btn-sm">Add Categories</button>
-                  </div>
-                </div>
-              </form>
-
-           </div>
-     </div>
 
         <!-- End Body ======================================= -->
 
-        </div>
-
-
-
     </div>
-    <!-- End SideBar And Body Page -->
+
+
+
+  </div>
+  <!-- End SideBar And Body Page -->
 
 
 </div>
@@ -104,6 +102,113 @@
 
 <!-- End Body  -->
 
+<?php
+      } elseif ($do == "insert") {
+
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+          //    Start Page Insert 
+
+          $table = "categories";
+
+          $msgerrors = array();
+
+          $verfiycode = rand(10000, 99999);
+
+          if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+            $name = superFilter($_POST['name']);
+
+            checkLength("category name",  $name, 2, 50);
+
+            $namear = $_POST['namear'];
+
+            checkLength("category name arabic",  $namear, 2, 50);
+
+            $desc    = superFilter($_POST['desc']);
+
+            checkLength("description",  $desc, 10, 250);
+
+            $descar    = superFilter($_POST['descar']);
+
+            checkLength("description",  $descar, 10, 250);
+
+ 
+            $data = getData("categories", "categories_name",  $name);
+
+            $count = $data['count'];
+
+            if ($count > 0) {
+
+?>
+
+      <div class="alert alert-warning"> category already existst</div>
 
 
-<?php include "../../include/footer.php";  ?>
+      <?php
+
+              // echo json_encode(array("status" => "faild", "cause" => "email Or phone already existst", "key" => "found"));
+
+            } else {
+
+              if (empty($msgerrors)) {
+
+                $values = array(
+                  "categories_name" => $name,
+                  "categories_name_ar" => $namear,
+                  "categories_desc" => $desc,
+                  "categories_desc_ar" => $descar 
+                );
+                $countinsert  = insertData($table, $values);
+                if ($countinsert > 0) {
+
+      ?>
+          <div class="alert alert-success"> Add Category Success </div>
+
+
+
+        <?php
+
+                  header("Location:categories.php");
+                  exit();
+                } else {
+                  echo json_encode(array("status" => "faild", "cause" => "Insert Faild", "key" => "insert"));
+                }
+              } else {
+
+                foreach ($msgerrors as $errors) {
+        ?>
+          <div class="mg-15  alert alert-warning"><?php echo $errors;  ?></div>
+<?php
+                }
+                // echo json_encode(array("status" => "faild", "cause" => $msgerrors, "key" => "insert"));
+              }
+            }
+          }
+          //    End Page Insert
+        } else {
+          echo "reuest Not post";
+        }
+      } else {
+
+        echo "Page Not Found";
+      }
+?>
+
+<?php include "../../include/footer.php";
+
+ob_flush();
+?>
+
+
+
+
+<!--  <div class="form-group">
+                  <label for="image" class="col-sm-2 control-label">image</label>
+                  <div class=" file-upload col-sm-10">
+                    <div class="file-select">
+                      <div class="file-select-button" id="fileName">Choose image</div>
+                      <div class="file-select-name" id="noFile">No image chosen...</div> 
+                      <input type="file" name="stdimage" id="chooseFile" placeholder="image">
+                    </div>
+                  </div>
+                </div> -->
--- pages/categories/categories.php
+++ pages/categories/categories.php
@@ -2,6 +2,22 @@
 <?php include "../../include/header.php"; ?>
 <?php include "../../include/navmobile.php";   ?>
 
+<?php 
+
+// Var Global For Every Page 
+
+$titlepage          = "Categories"                 ; 
+$linkaddpage        = "addcategories.php"          ;  
+$titleaddpage       = "Add Categories"             ; 
+$linkeditpage       = "editcategories.php"         ;
+$linkdeletepage     = "deletecategories.php"       ;
+
+
+
+
+
+?>
+
 
 <!-- Start Body  -->
 
@@ -35,19 +51,20 @@
 
             <div class="panel panel-default panel-custom manage">
                 <div class="panel-heading">
-                    <h3 class="panel-title"> Users </h3>
+                    <h3 class="panel-title"><?php echo $titlepage ;?></h3>
                 </div>
                 <div class="panel-body">
                     <div class="row m-b">
                         <div class="col-xs-6 col-sm-9 ">
-                            <a href="addusers.php" class="btn btn-success btn-sm">+ Add users</a>
+                            <a href="<?=$linkaddpage ?>" class="btn btn-success btn-sm">+ <?php echo $titleaddpage ;   ?></a>
                         </div>
-                        <div class="col-xs-6 col-sm-3 col-pull-right">
+                        <!-- Start Choose Select -->
+                        <!-- <div class="col-xs-6 col-sm-3 col-pull-right">
 
                             <form action="" method="get" class="parent-search m-b">
 
                                 <select name="searchclass" class="niceselect wide">
-                                    <option value="0">choose class</option>
+                                    <option value="0">choose class</option> -->
                                     <?php
                                     // foreach ($classsearch as $stdclass) {
                                     //     echo "<option value='" . $stdclass['class_id'] . "'";
@@ -59,24 +76,21 @@
                                     //     echo ">" .  $stdclass['class_name'] . "</option>";
                                     // }
                                     ?>
-                                </select>
+                                <!-- </select>
                                 <button type="submit" class="btn btn-danger"><i class="fa fa-search"></i></button>
 
                             </form>
                             <div class="clearfix m-b"></div>
-
-
-
-
-                        </div>
+                        </div> -->
+                        <!-- End Choose Select -->
                     </div>
                     <div class="clearfix"></div>
 
                     <!-- start pager -->
                     <ul class="nav nav-tabs m-b pager-section">
-                        <li class="active"><a href="" data-target="all" class="all">All users</a> </li>
-                        <li> <a href="" data-target="wait">wait approve</a> </li>
-                        <li> <a href="" data-target="approved">approved</a> </li>
+                        <li class="active"><a href="" data-target="all" class="all">All Categories</a> </li>
+                        <!-- <li> <a href="" data-target="wait">wait approve</a> </li>
+                        <li> <a href="" data-target="approved">approved</a> </li> -->
                     </ul>
                     <!-- End pager -->
 
@@ -84,13 +98,13 @@
                     <form action="<?= $_SERVER['PHP_SELF']; ?> " method="get" class="parent-search m-b">
                         <div class="row">
                             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-                                <input type="text" class="form-control input-sm" id="search" placeholder="Search" name="searchstd">
+                                <input type="text" class="form-control input-sm" id="search" placeholder="Search" name="search">
                             </div>
                             <div class="col-xs-12 col-sm-6 col-md-8 col-lg-9">
                                 <?php
-                                if (isset($_GET['searchstd'])) {
-                                    $get = $_GET['searchstd'];
-                                    $and = "And users_name  LIKE  '%$get%' ";
+                                if (isset($_GET['search'])) {
+                                    $get = $_GET['search'];
+                                    $and = "And (categories_name  LIKE  '%$get%') OR (categories_name_ar  LIKE  '%$get%') ";
                                 } else {
                                     $and = null;
                                 };
@@ -105,53 +119,37 @@
                         <thead>
                             <tr>
                                 <td>name</td>
-                                <td class="hidden-xs">phone</td>
-                                <td class="hidden-xs">email</td>
+                                <td class="hidden-xs">name ar</td>
+                               
 
                                 <td>control</td>
                             </tr>
                         </thead>
 
                         <?php
-                        $users = getAllData("users", "users_role != 1 $and ORDER BY users_id DESC")['values'];
+                        $categories = getAllData("categories", "1 = 1  $and ORDER BY categories_id DESC")['values'];
 
 
-                        foreach ($users as $user) {
+                        foreach ($categories as $category) {
                         ?>
                             <tr>
-                                <td class=" "><?php echo $user['users_name']
+                                <td class=" "><?php echo $category['categories_name']
                                                 ?></td>
-                                <td class="hidden-xs <?php if ($user['users_approve'] == 1) {
-                                                            echo "approved";
-                                                        } else {
-                                                            echo "wait";
-                                                        };
-                                                        ?>"><?php echo $user['users_phone']
-                                                            ?></td>
-                                <td class="hidden-xs <?php if ($user['users_approve'] == 1) {
-                                                            echo "approved";
-                                                        } else {
-                                                            echo "wait";
-                                                        };
-                                                        ?>"><?php echo $user['users_email']
-                                                            ?></td>
+                                <td class="hidden-xs "><?php echo $category['categories_name_ar']
+                                                        ?></td>
+                                 
 
                                 <td>
-                                    <a href="users.php?do=edit&stdid=<?php   //echo $user['std_id']  
+                                    <a href="<?= $linkeditpage ?>?do=edit&stdid=<?php   //echo $user['std_id']  
                                                                         ?>" class="btn-primary btn-sm mg-h-5 <?php //echo $user['std_section']; 
                                                                                                                 ?>"> <i class="fa fa-edit"> </i> <span class="hidden-xs">Edit</span> </a>
 
 
-                                    <a href="deleteusers.php?userid=<?php echo  $user['users_id']
+                                    <a href="<?= $linkdeletepage ?>?userid=<?php echo  $user['users_id']
                                                                     ?>" class="btn-danger btn-sm mg-h-5 <?php //echo $user['std_section']; 
                                                                                                         ?>"><i class="fa fa-remove"> </i> <span class="hidden-xs">Delete</span> </a>
 
-                                    <?php if ($user['users_approve'] == "0") {  ?>
 
-                                        <a href="approveusers.php?userid=<?php echo  $user['users_id']
-                                                                            ?>" class="btn-info btn-sm mg-h-5 <?php //echo $user['std_section']; 
-                                                                                                                ?>"><i class="fa fa-check"></i> <span class="hidden-xs">Approve</span> </a>
-                                    <?php } ?>
                                 </td>
                             </tr>
 
