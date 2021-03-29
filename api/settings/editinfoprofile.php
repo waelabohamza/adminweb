23bd9e8055be1e664ed182a54e1c8a22887b4f1f
waelabohamza <waeleagle1243@gmail.com>
2021-03-29 19:41:20 +0300

    refresh

--- api/settings/editinfoprofile.php
+++ api/settings/editinfoprofile.php
@@ -1,10 +1,35 @@
 <?php
 include "../../connect.php";
+
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
    
-    $id       =  superFilter($_POST['id']) ; 
-    $username =  superFilter($_POST['username']) ; 
-    $password =  superFilter($_POST['password']) ; 
+   if (isset($_POST['username'])) {
+
+    $username = superFilter($_POST['username'])  ; 
+
+    $id = superFilter($_POST['id'])  ; 
+
+    $data = array("users_name" =>    $username) ;  
+
+    $count = updateData("users" ,$data  , "users_id = '$id' ") ; 
+
+    countresault($count) ; 
+
+   }
+   
+   if (isset($_POST['password'])) {
+
+    $password = sha1($_POST['password'])  ; 
+
+    $id = superFilter($_POST['id'])  ; 
+
+    $data = array("users_password" =>    $password) ;  
+
+    $count = updateData("users" ,$data  , "users_id = '$id' ") ; 
+
+    countresault($count) ; 
+
+    }
 
 
 } else {
